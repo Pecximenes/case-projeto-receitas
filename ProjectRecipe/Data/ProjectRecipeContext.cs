531f64f0c0e043da739c611b20d940d970590cbf
Pedro Ximenes <pedroximenes76@gmail.com>
2025-04-23 16:05:18 -0300

    🗃️ ✨ Creating First migration and database

--- ProjectRecipe/Data/ProjectRecipeContext.cs
+++ ProjectRecipe/Data/ProjectRecipeContext.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ProjectRecipe.Models;
+
 namespace ProjectRecipe.Data;
 
-public class ProjectRecipeContext
+public class ProjectRecipeContext : DbContext
 {
-    
+    public DbSet<ProjectRecipeModel> Ingredient { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=projectRecipe.sqlite");
+        base.OnConfiguring(optionsBuilder);
+    }
 }
--- ProjectRecipe/obj/Debug/net8.0/ProjectRecipe.AssemblyInfo.cs
+++ ProjectRecipe/obj/Debug/net8.0/ProjectRecipe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectRecipe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+041a4f1508909077369b510fef08a4cca15811b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dc985225b511b7b6452c83bb1fef2476cabdc25")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectRecipe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectRecipe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
